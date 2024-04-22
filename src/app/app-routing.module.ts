8618c51fb1b127b8107f8b671d60102b8a6b54ba
Luis Perez <U20207297@utp.edu.pe>
2024-04-22 11:51:09 -0500

    Se creo el sistema de Ruteo (ver Producto), boton "Volver"

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+// Componentes
+import { ListadoObjetoComponent } from './components/listado-objeto/listado-objeto.component';
+import { AgregarEditarObjetoComponent } from './components/agregar-editar-objeto/agregar-editar-objeto.component';
+import { VerObjetoComponent } from './components/ver-objeto/ver-objeto.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'listarProducto', pathMatch: 'full' },
+  { path: 'listarProducto', component: ListadoObjetoComponent },
+  { path: 'agregarProducto', component: AgregarEditarObjetoComponent },
+  { path: 'verProducto/:id', component: VerObjetoComponent },
+  { path: 'editarProducto/:id', component: AgregarEditarObjetoComponent },
+  { path: '**', redirectTo: 'listarProducto', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
